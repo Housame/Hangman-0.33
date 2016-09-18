07fb9c937685eac6159354a6f0606e70c9e0cbde
Housame Oueslati <housame.oueslati@hotmail.com>
2016-09-18 16:30:15 +0200

    Player class

---
+++ Hangman 0.33/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman_0._33
+{
+    class Player
+    {
+        public string name;
+
+        public Player (string name)
+        {
+            Name = name;
+        }
+        
+
+        public string Name
+        {
+            get
+            { return name; }
+            set
+            { name = value; }
+        }
+
+    }
+}
